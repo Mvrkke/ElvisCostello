660093e61c5de3ec940c01b2694fbd106b54d7cf
Mvrkke <mvrkke@outlook.com>
2025-03-14 17:35:05 +0100

    Initial Commit

---
+++ ElvisCostello/ElvisCostelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloApp.swift
+//  ElvisCostello
+//
+//  Created by Sharmarke Mohamed Hilowle on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
